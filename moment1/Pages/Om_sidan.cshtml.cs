3ce4301ffa84ad632b3ba14385f478d702c34f53
hejmdahl <moahjemdahl@outlook.com>
2021-01-23 14:41:45 +0100

    RazorGrunder

---
+++ moment1/Pages/Om_sidan.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace moment1.Pages
+{
+    public class Om_sidanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ moment1/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var endOfCourseDate = new Date("Mars 20, 2021 23:59:59").getTime();
+
+// Nedräkning
+var x = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    // Tidsskillnaden mellan slut- och nutid
+    var difference = endOfCourseDate - now;
+
+    // Kalkylering av tid
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // Skriv ut resultat resultat
+    document.getElementById("timer").innerHTML = days + " dagar, " + hours + " timmar, "
+        + minutes + " minuter och " + seconds + " sekunder ";
+}, 1000);
